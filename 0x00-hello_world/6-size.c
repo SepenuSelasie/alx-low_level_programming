20df34d7cf515e04399f5c70fd9ac338750fc574
SepenuSelasie <developerseps5@gmail.com>
2023-02-20 12:14:33 -0800

    Display the sie of various data typoes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This file determined the size of the various data types
+ *
+ * Return: Always 0 (Success)
+ */
+int  main(void)
+{
+	sizeof (int);
+	sizeof (char);
+	sizeof (float);
+	sizeof (long int);
+	sizeof (long long int);
+		return (0);
+}
