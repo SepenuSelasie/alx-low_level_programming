0f2627cc03781211f307d01ed28800ee5e846ad3
SepenuSelasie <developerseps5@gmail.com>
2023-02-20 08:03:06 -0800

    My seond C program ever

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:This program prints the specified string using printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
