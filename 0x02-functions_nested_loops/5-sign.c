cfa9734e03ffc3dc33bc14b6566fe78d14426542
SepenuSelasie <developerseps5@gmail.com>
2023-02-22 06:47:28 -0800

    This program determines whther or not an integer is positive oir negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ *@n: n ian any integer
+ *Return: 1 and + for positive numbers, -1 and - for negative numbers and 0 0 for anything else
+ */
+
+int print_sign(int n)
+{
+
+		if (n > 0)
+		{
+			_putchar('+');
+			return (1);
+		}
+		else if (n == 0)
+		{
+			_putchar('0');
+			return (0);
+		}
+		else 
+		{
+			_putchar('-');
+			return (1);
+		}
+}
