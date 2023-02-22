f251d272baeb4a55c7c28dbf69d45015f2493743
SepenuSelasie <developerseps5@gmail.com>
2023-02-22 07:09:28 -0800

    Ensure there are no white trailing spaces

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number
- *@n: n ian any integer
- *Return: 1 and + for positive numbers, -1 and - for negative numbers 
- */
+* print_sign - Prints the sign of a number
+*@n: n ian any integer
+*
+* Return: 1 for postive, -1 for negative
+*/
 
 int print_sign(int n)
 {
-
-		if (n > 0)
-		{
-			_putchar('+');
-			return (1);
-		}
-		else if (n == 0)
-		{
-			_putchar('0');
-			return (0);
-		}
-		else 
-		{
-			_putchar('-');
-			return (1);
-		}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
+
