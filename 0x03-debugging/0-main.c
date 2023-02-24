995a229f4c5bc1333910ec47e3ca4a9f553c22e7
SepenuSelasie <developerseps5@gmail.com>
2023-02-24 01:46:52 -0800

    Test for a postive or negative integer

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests if an integer given is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(1);
+
+	return (0);
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is a directory containin g a series of projects based on the conmcept of debugging and ensuring that effective coes are written.
